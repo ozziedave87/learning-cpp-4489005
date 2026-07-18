ac552f97cfcd085620ba4c8573e8dee839ed6bfe
ozziedave87 <113081262+ozziedave87@users.noreply.github.com>
2026-07-18 02:00:27 +0000

    Day 2

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -1,21 +1,29 @@
-/* Chapter 1 notes
-
- key takeaways from the video:
-
-Object-oriented programming in C++ is centered around modeling real-world things as classes, which contain data and functions.
-
-Encapsulation means keeping data and operations within their models, ensuring that each class handles its own information and behavior.
-
-Classes have members that can be public, private, or protected, controlling access to data and functions.
-
-Inheritance allows classes to derive from other classes, sharing and extending functionality.
-
-Polymorphism lets subclasses implement inherited functions differently, enabling flexible and dynamic behavior.
-
-These concepts form the foundation of C++ programming and are essential for building organized and reusable code.
-
-*/
-
 // Learning C++ 
 // Exercise 01_02
 // Hello World, by Eduardo Corpeño 
+
+
+#include <iostream>
+
+/*int main(){
+    std::cout << "Hi There!" << std::endl;
+
+    std::cout << std::endl << std::endl;
+    return (0);
+}*/
+    
+#include <iostream>
+
+int main(){
+    std::cout << "Hi there!" << std::endl;
+    std::cout << std::endl << std::endl;
+    return(0);
+}
+/*#include <iostream>
+
+int main(){
+    std::cout << "Hi There!" << std::endl;
+
+    std::cout <<std::endl << std::endl;
+    return (0);
+}*/
--- src/Ch01/01_03/CodeDemo.cpp
+++ src/Ch01/01_03/CodeDemo.cpp
@@ -2,7 +2,7 @@
 // Challenge 01_03
 // Console Interaction, by Eduardo Corpeño 
 
-#include <iostream>
+/*#include <iostream>
 #include <string>
 
 int main(){
@@ -12,4 +12,26 @@ int main(){
 
     std::cout << std::endl << std::endl;
     return (0);
+}*/
+
+// My answer to ask for name and display name
+
+#include <iostream>
+#include <string>
+
+int main(){
+    
+    //Variables
+    std::string strVar;
+
+    //Function
+    std::cout << "What is your name?" << std::flush; //flush mkes sure it is sent to terminal before asking for next step
+    std::cin >> strVar;
+    std::cout << "Nice to meet you " << strVar << "!" <<std::endl; //NB cout only works for a single word, no space
+
+    std::cout << std::endl << std::endl;
+
+    return(0);
 }
+
+
