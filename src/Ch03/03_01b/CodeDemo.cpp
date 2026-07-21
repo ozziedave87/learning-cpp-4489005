593d6b281bab551aaa20f621f0739fcff188f619
ozziedave87 <113081262+ozziedave87@users.noreply.github.com>
2026-07-21 20:17:54 +0000

    20260722

--- src/Ch03/03_01b/CodeDemo.cpp
+++ src/Ch03/03_01b/CodeDemo.cpp
@@ -5,8 +5,28 @@
 #include <iostream>
 #include <string>
 
+enum class cow_purpose {dairy, meat, hide, pet};
+
+struct cow {
+    std::string name;
+    int age;
+    cow_purpose purpose;
+};
+
 int main(){
     
+    cow my_cow;
+    my_cow.age = 5;
+    my_cow.name = "Betsy";
+    my_cow.purpose = cow_purpose::dairy;
+
+    //Print cow type
+    std::cout << my_cow.name << "is a type - "<< (int)my_cow.purpose << " cow." << std::endl;
+
+    //Print cow age
+    std::cout << my_cow.name << " is " << my_cow.age << " years old." << std::endl;
+
+
     std::cout << std::endl << std::endl;
     return (0);
 }
--- src/Ch03/03_02b/CodeDemo.cpp
+++ src/Ch03/03_02b/CodeDemo.cpp
@@ -7,20 +7,62 @@
 
 enum class cow_purpose {dairy, meat, hide, pet};
 
-struct cow{
+class cow{ //NB struct was changed to class
+public:
+//constructor
+    cow(std::string name_i, int age_i, cow_purpose purpose_i){
+        name = name_i;
+        age = age_i;
+        purpose = purpose_i;
+    }
+//Getter functions
+    std::string get_name()const{ 
+        return name;
+    }
+    int get_age() const{ 
+        return age;
+    }
+    cow_purpose get_purpose() const{ 
+        return purpose;
+    }
+//Setter fucntions
+    void set_name(std::string new_name){
+        name = new_name;
+    }
+    void set_age(int new_age){
+        age = new_age;
+    }
+    void set_purpose(cow_purpose new_purpose){
+        purpose = new_purpose;
+    }
+private: //makes public or private
     std::string name;
     int age;
     cow_purpose purpose;
 };
 
 int main(){
-    cow my_cow;
+    /*cow my_cow;
     my_cow.age = 5;
     my_cow.name = "Betsy";
-    my_cow.purpose = cow_purpose::dairy;
-    std::cout << my_cow.name << " is a type-" << (int)my_cow.purpose << " cow." << std::endl;
-    std::cout << my_cow.name << " is " << my_cow.age << " years old." << std::endl;
-    
+    my_cow.purpose = cow_purpose::dairy;*/
+
+    cow my_cow("Hildy", 7, cow_purpose::pet);
+
+    //Getter outputs
+    std::cout << "Getter outputs" << std::endl;
+    std::cout << my_cow.get_name() << " is a type - " << (int)my_cow.get_purpose() << " cow." << std::endl;
+    std::cout << my_cow.get_name() << " is " << my_cow.get_age() << " years old." << std::endl;
+
+    //Setter outputs
+    std::cout << std::endl << std::endl;
+    //cow my_cow2;
+    my_cow.set_name("Eddy");
+    my_cow.set_age(12);
+    my_cow.set_purpose(cow_purpose::dairy);
+     std::cout << my_cow.get_name() << " is a type - " << (int)my_cow.get_purpose() << " cow." << std::endl;
+    std::cout << my_cow.get_name() << " is " << my_cow.get_age() << " years old." << std::endl;
+
     std::cout << std::endl << std::endl;
     return (0);
 }
